8cc9eac3a2170521fbf3032b37360e389a673878
HusseinSaeid <55005651+HusseinSaeid@users.noreply.github.com>
2025-09-30 00:42:08 +0300

    Update Navebar.tsx

--- components/Navebar.tsx
+++ components/Navebar.tsx
@@ -5,11 +5,17 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <Link href="/">
-        <p className="text-2xl font-bold text-gradient">Resumix</p>
+        <p className="text-lg font-bold text-gradient">Resumix</p>
       </Link>
       <div className="flex items-center gap-4">
-        <Link href="/upload" className="primary-button w-fit">
-          Upload Resume
+        <Link
+          href="/upload"
+          className="primary-gradient text-center backdrop-blur-md shadow-sm text-white rounded-full
+         px-3 py-1.5 text-sm w-full 
+         sm:w-auto sm:px-5 sm:py-2 sm:text-base
+         font-semibold transition-all duration-300 hover:shadow-lg"
+        >
+          Upload
         </Link>
         <SignOutButton
           variant="ghost"
